a9d1d789091aa14c8f08235e504ce82e656efb5c
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-02-19 00:02:41 -0500

    all functionality except game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,73 @@
 $(document).ready(function() {
 
-//random number to guess and gem number generation
+var wins = 0;
+var losses = 0;
+var score = 0;
 
-    var randomNumber = Math.floor((Math.random() * 120) + 1);
+//generates random number for each star
+var randomNumber = function() {
+    Math.floor((Math.random() * 120) + 1);
     console.log(randomNumber);
     $("#numberGoal").text(randomNumber);
+}
 
-    var starNum = function() {
-        return Math.floor((Math.random() * 12) + 1);
+randomNumber();
+
+var starNum = function() {
+    return Math.floor((Math.random() * 12) + 1);
+}
+
+var redStar = starNum();
+var blackStar = starNum();
+var yellowStar = starNum();
+var greenStar = starNum();
+
+var winOrLose = function () {
+    if (score === randomNumber) {
+        alert("Yay!  You've saved Earth!");
+        wins++;
+        $("#wins").text(wins);
+        score = 0;
+        $("#score").text(score);
+        //randomNumber();
     }
+    else if (score > randomNumber) {
+        alert("Oh noes!!!  Planet Earth has been destroyed!");
+        losses++;
+        $("#losses").text(losses);
+        score = 0;
+        $("#score").text(score);
+        //randomNumber();
+    }
+    if ((score === randomNumber) || (score > randomNumber)) {
+        randomNumber();
+    }
+}
 
-    var redStar = crystalNum();
-    var blackStar = crystalNum();
-    var yellowStar = crystalNum();
-    var greenStar = crystalNum();
+//gem clicks and display to total score
+$("#redStar").on("click", function() {
+    score = score + redStar;
+    console.log(score);
+    $("#score").text(score);
+    winOrLose();
+});
 
-    var wins = 0;
-    var losses = 0;
-    var score = 0;
+$("#blackStar").on("click", function() {
+    score = score + blackStar;
+    $("#score").text(score);
+    winOrLose();
+});
 
+$("#yellowStar").on("click", function() {
+    score = score + yellowStar;
+    $("#score").text(score);
+    winOrLose();
+});
+
+$("#greenStar").on("click", function() {
+    score = score + greenStar;
+    $("#score").text(score);
+    winOrLose();
+    });
+
+});
